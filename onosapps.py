1ebec297d78d6fbb68b002b7ea3cd21a0aaf0c58
Aladejebi Ebenezer <ea22abd@herts.ac.uk>
2023-09-06 12:48:32 +0100

    few changes to onsapp api

--- onosapps.py
+++ onosapps.py
@@ -13,11 +13,11 @@ def run_pingall():
 
         onos_cli.sendline('app activate org.onosproject.openflow')
         onos_cli.expect('onos.*> ', timeout=120)  # Increased timeout for pingall command
-        print("fwd activated")
+        print("openflow activated")
 
         onos_cli.sendline('app activate org.onosproject.gui2')
         onos_cli.expect('onos.*> ', timeout=120)  # Increased timeout for pingall command
-        print("fwd activated")
+        print("gui2 activated")
 
         onos_cli.sendline('exit')  # Exit the ONOS CLI
         onos_cli.expect(pexpect.EOF)
