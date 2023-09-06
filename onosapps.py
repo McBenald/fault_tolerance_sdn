84a211f09ab17fb3728db2925df1b0b24adefc21
Aladejebi Ebenezer <ea22abd@herts.ac.uk>
2023-09-06 11:53:35 +0100

    timeout added to remove incessant loop

--- onosapps.py
+++ onosapps.py
@@ -8,15 +8,15 @@ def run_pingall():
         print("Connected to ONOS CLI")
 
         onos_cli.sendline('app activate org.onosproject.fwd')
-        #onos_cli.expect('onos.*> ', timeout=120)  # Increased timeout for pingall command
+        onos_cli.expect('onos.*> ', timeout=120)  # Increased timeout for pingall command
         print("fwd activated")
 
         onos_cli.sendline('app activate org.onosproject.openflow')
-        #onos_cli.expect('onos.*> ', timeout=120)  # Increased timeout for pingall command
+        onos_cli.expect('onos.*> ', timeout=120)  # Increased timeout for pingall command
         print("fwd activated")
 
         onos_cli.sendline('app activate org.onosproject.gui2')
-        #onos_cli.expect('onos.*> ', timeout=120)  # Increased timeout for pingall command
+        onos_cli.expect('onos.*> ', timeout=120)  # Increased timeout for pingall command
         print("fwd activated")
 
         onos_cli.sendline('exit')  # Exit the ONOS CLI
