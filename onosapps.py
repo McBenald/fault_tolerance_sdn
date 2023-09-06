e18417937985515401b6793ea04bda54fa1920dc
Aladejebi Ebenezer <ea22abd@herts.ac.uk>
2023-09-06 11:52:21 +0100

    install needed onos apps

---
+++ onosapps.py
@@ -0,0 +1,29 @@
+import pexpect
+
+def run_pingall():
+    try:
+        # Start ONOS CLI and execute 'pingall' command
+        onos_cli = pexpect.spawn('onos localhost')
+        onos_cli.expect('onos.*> ', timeout=60)  # Increased timeout to 60 seconds
+        print("Connected to ONOS CLI")
+
+        onos_cli.sendline('app activate org.onosproject.fwd')
+        #onos_cli.expect('onos.*> ', timeout=120)  # Increased timeout for pingall command
+        print("fwd activated")
+
+        onos_cli.sendline('app activate org.onosproject.openflow')
+        #onos_cli.expect('onos.*> ', timeout=120)  # Increased timeout for pingall command
+        print("fwd activated")
+
+        onos_cli.sendline('app activate org.onosproject.gui2')
+        #onos_cli.expect('onos.*> ', timeout=120)  # Increased timeout for pingall command
+        print("fwd activated")
+
+        onos_cli.sendline('exit')  # Exit the ONOS CLI
+        onos_cli.expect(pexpect.EOF)
+        print("Exited ONOS CLI")
+    except pexpect.ExceptionPexpect as e:
+        print("Error:", e)
+
+if __name__ == '__main__':
+    run_pingall()
