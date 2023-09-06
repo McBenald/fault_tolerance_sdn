e7ef9919b45256173bc1a845e04b71907f334a59
Aladejebi Ebenezer <ea22abd@herts.ac.uk>
2023-09-06 13:29:04 +0100

    syntax change

--- standby_switch.py
+++ standby_switch.py
@@ -4,7 +4,7 @@ def set_switch_to_standby(switch_dpid):
     # ONOS REST API URL to set the switch to standby
     onos_ip = "127.0.0.1"  # Replace with your ONOS IP address
     onos_port = "8181"         # Replace with your ONOS port
-    url = f"http://{onos_ip}:{onos_port}/onos/v1/devices/{switch_dpid}/role/STANDBY"
+    url = "http://{}:{}/onos/v1/devices/{}/role/STANDBY".format(onos_ip, onos_port, switch_dpid)
 
     # Send a PUT request to set the switch to standby
     response = requests.put(url)
